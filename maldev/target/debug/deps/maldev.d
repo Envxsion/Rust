b929975894800c7a36c200d7310d539b84dd972a
Envxsion <74938513+Envxsion@users.noreply.github.com>
2024-06-02 23:13:09 +1000

    update

---
+++ maldev/target/debug/deps/maldev.d
@@ -0,0 +1,5 @@
+C:\Users\cyn0v\Documents\GitHub\Rust\maldev\target\debug\deps\maldev.exe: src\main.rs
+
+C:\Users\cyn0v\Documents\GitHub\Rust\maldev\target\debug\deps\maldev.d: src\main.rs
+
+src\main.rs:
---
+++ maldev/target/debug/maldev.d
@@ -0,0 +1,1 @@
+C:\Users\cyn0v\Documents\GitHub\Rust\maldev\target\debug\maldev.exe: C:\Users\cyn0v\Documents\GitHub\Rust\maldev\src\main.rs
