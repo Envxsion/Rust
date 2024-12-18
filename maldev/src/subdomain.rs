82b0fd7e454f96015e1dcae687d6e19fc2ff8e31
Envxsion <74938513+Envxsion@users.noreply.github.com>
2024-12-18 11:17:20 +0530

    rustlings ftw 04_prim_1

--- maldev/src/subdomain.rs
+++ maldev/src/subdomain.rs
@@ -1,8 +1,7 @@
-use std::error::Error;
+use std::{error::Error, time::Duration};
 
 pub fn subd_s(domain: &str) -> Result<(), Box<dyn Error>> {
     // Implement your subdomain scanning logic here
     println!("Scanning subdomains for domain: {}", domain);
-    // Dummy implementation for illustration
-    Ok(())
+    
 }
